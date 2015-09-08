cc1bba107f6bf22fdcb8bf1fb651f1b436746934
Tony Bowden <tony@tmtm.com>
2015-09-08 20:00:35 +0100

    initial scraper

---
+++ scraper.rb
@@ -0,0 +1,52 @@
+#!/bin/env ruby
+# encoding: utf-8
+
+require 'scraperwiki'
+require 'yaml'
+
+class String
+  def tidy
+    self.gsub(/[[:space:]]+/, ' ').strip
+  end
+end
+
+def yaml_at(url)
+  YAML.load(open(url).read)
+end
+
+def gender_from(str)
+  return if str.empty?
+  return 'male' if str.downcase == 'm'
+  return 'female' if str.downcase == 'f'
+  raise "unknown gender: #{str}"
+end
+
+def scrape_list(url)
+  yaml_at(url).each do |person|
+    # TODO any in current Term
+    term = person['terms'].last
+    data = { 
+      id: person['id']['bioguide'],
+      wikipedia__en: person['id']['wikipedia'],
+      name: person['name']['official_full'],
+      image: "https://theunitedstates.io/images/congress/original/#{person['id']['bioguide']}.jpg",
+      given_name: person['name']['first'],
+      family_name: person['name']['last'],
+      sort_name: "%s, %s" % [ person['name']['last'], person['name']['first'] ],
+      birth_date: person['bio']['birthday'],
+      gender: gender_from(person['bio']['gender']),
+      house: term['type'],
+      start_date: term['start'],
+      state: term['state'],
+      district: term['district'],
+      party: term['party'],
+      homepage: term['url'],
+      address: term['address'],
+      phone: term['phone'],
+      fax: term['fax'],
+    }
+    ScraperWiki.save_sqlite([:id], data)
+  end
+end
+
+scrape_list('https://raw.githubusercontent.com/unitedstates/congress-legislators/master/legislators-current.yaml')
